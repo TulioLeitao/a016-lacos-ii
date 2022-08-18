7ae539a58a1502f3097186305c8ac16f33f26012
TulioLeitao <tulioleitao@gmail.com>
2022-08-18 13:24:57 -0300

    Aula a016

---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+const mana = ["Branca", "Azul", "Preta", "Vermelha", "Verde"];
+
+for (let i = 0; i < mana.length; i++){
+
+    console.log (`${i+1} - ${mana[i]}`)
+    
+   
+};
+
+// não entendi como fazer
